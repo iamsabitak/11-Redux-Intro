6050970466ac60f701d156665f75a350ffedd081
sabita <sabitakhadka091@gmail.com>
2023-11-23 21:07:28 +0545

    Integrate Redux store into the main application

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "./store";
+import store from  "./store";
+import { deposit } from "./features/accounts/accountSlice";
+
+// store.dispatch(deposit(300));
+// console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
