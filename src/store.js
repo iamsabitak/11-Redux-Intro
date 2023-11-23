087f35396348b457a352478d4d8c2d96520f26ca
sabita <sabitakhadka091@gmail.com>
2023-11-23 21:03:27 +0545

    Refactor Redux store setup with combineReducers

--- src/store.js
+++ src/store.js
@@ -3,8 +3,8 @@ import customerReducer from "./features/customers/customerSlice";
 import accountReducer from "./features/accounts/AccountSlice";
 
 const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+   account: accountReducer,
+   customer: customerReducer,
 });
 
 const store = createStore(rootReducer);
