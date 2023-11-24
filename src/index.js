9f7f0825e4b035dd61e2d54a791b113b01f9dde1
sabita <sabitakhadka091@gmail.com>
2023-11-24 12:10:41 +0545

    imported the Provider from redux and wrapped thr app component

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import store from  "./store";
+import { Provider } from "react-redux";
 import { deposit } from "./features/accounts/accountSlice";
 
-// store.dispatch(deposit(300));
-// console.log(store.getState());
+store.dispatch(deposit(300));
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Provider>
 );
