0ced6bc3011b1528de21104fd5ceff639c94b314
sabita <sabitakhadka091@gmail.com>
2023-11-24 17:01:27 +0545

    Implemented account operation functionalities with loading state

--- src/features/accounts/AccountOperations.js
+++ src/features/accounts/AccountOperations.js
@@ -13,14 +13,17 @@ function AccountOperations() {
   const {
     loan: currentLoan,
     loanPurpose: currentLoanPurpose,
-    balance,
+    isLoading,
+    // balance,
   } = useSelector((store) => store.account);
-  console.log(balance);
+  // console.log(balance);
 
   function handleDeposit() {
     if (!depositAmount) return;
-    dispatch(deposit(depositAmount));
+    
+    dispatch(deposit(depositAmount, currency));
     setDepositAmount("");
+    setCurrency("USD");
   }
 
   function handleWithdrawal() {
@@ -60,7 +63,9 @@ function AccountOperations() {
             <option value="GBP">British Pound</option>
           </select>
 
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
+          <button onClick={handleDeposit} disabled={isLoading}>
+            {isLoading ? "Converting...." : `Deposit ${depositAmount}`}
+          </button>
         </div>
 
         <div>
